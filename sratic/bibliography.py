02e0b3f4779df97f77164e3b7fe05b09e9149fb6
Christian Dietrich <stettberger@dokucode.de>
2020-04-09 14:27:41 +0200

    Insert an assert to find invalid bibtex entries

--- sratic/bibliography.py
+++ sratic/bibliography.py
@@ -148,11 +148,13 @@ def load_bibtex(filename, modify_data=None):
                 continue
 
             field_p = field + "s"
+            assert field_p not in e, f"Invalid Bibtex field: {field_p} in entry {e['id']}"
             e[field_p] = [x.strip() for x in e[field].replace('\n', ' ').split(' and ')]
             for i, a in enumerate(e[field_p]):
                 if ',' in a:
                     a = a.split(',', 1)
                     e[field_p][i] = ("%s %s" % (a[1], a[0])).strip()
+
 
 
     fd.close()
